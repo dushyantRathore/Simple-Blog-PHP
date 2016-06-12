9ef0b4eb8e0c3c7dd4e9d320b9955f5488792035
Dushyant Rathore <dushyant.bgs@gmail.com>
2016-06-12 13:37:17 +0530

    Update add_post.php

--- add_post.php
+++ add_post.php
@@ -1,60 +1,65 @@
-<?php
+<?php 
 
-include_once("config.php");
+include_once("blog_main.php");
 
+//$date = date("m/d/Y h:i:s ", time());
+//echo $title."<br>".$content."<br>".$category."<br>".$date;
 
-if(isset($_POST['post_title'], $_POST['post_content'], $_POST['post_category']))
+if(isset($_POST['title'], $_POST['content'], $_POST['category']))
 {
-$title = trim($_POST['post_title']);
-$content = ($_POST['content']);
-$category = trim($_POST['post_category']);
 
-if(empty($title))
-{
-echo "Title Cannot be empty";
+	$title = $_POST['title'];
+	$content = $_POST['content'];
+	$category = $_POST['category'];
+
+	if(empty($title))
+	{
+		echo "Title Cannot be empty";
+	}
+	else if (strlen($title) >255)
+	{
+		echo "The Title Length cannot be greater than 255 characters";
+	}
+	else if(empty($content))
+	{
+		echo "Please enter some content";
+	}
+	else if (empty($category))
+	{
+		echo "Please add a category to the post";
+	}
+	else if(!empty($title) && !empty($content) && !empty($category))
+	{
+		add_post($title,$content,$category);
+	}
 }
-else if (strlen($title) >255)
-{
-echo "The Title Length cannot be greater than 255 characters";
-}
-else if(empty($content))
-{
-echo "Please enter some content";
-}
-else if (empty($category))
-{
-echo "Please add a category to the post";
-}
-else if(!empty($title) && !empty($content) && !empty($category))
-{
-add_post($title,$content,$category);
-}
-}
+
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<title>Add Post</title>
+	<title></title>
 </head>
+
 <body>
-<h1><u>Add Post</u></h1>
-<form action = "add_post.php" method="post">
-<label>Title</label>
-<br>
-<input type = "text" name = "post_title">
-<br>
-<label>Content</label>
-<br>
-<textarea name = "content" rows="8" cols="50"></textarea>
-<br>
-<label>Category</label>
-<br>
-<input type="text" name= "post_category">
-<br>
-<br>
-<input type="submit" name ="add" value ="Add Post">
-</form>
+	<h1><u>Add your Post</u></h1>
+	<form method="post" action="add_post.php">
+		<label>Title</label>
+		<br>
+		<input type= "text" name="title">
+		<br>
+		<label>Content</label>
+		<br>
+		<textarea name="content" rows="10" cols="50"></textarea>
+		<br>
+		<label>Category</label>
+		<br>
+		<input type="text" name="category">
+		<br>
+		<br>
+		<input type="submit" value="Submit">
+	</form>
 </body>
 </html>
