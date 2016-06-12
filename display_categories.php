a53a8645fd10ab0051d6fa39a26d8ab533a130ec
Dushyant Rathore <dushyant.bgs@gmail.com>
2016-06-13 00:51:46 +0530

    Update display_categories.php

--- display_categories.php
+++ display_categories.php
@@ -7,17 +7,37 @@ $query_run = mysql_query($sql);
 
 if(! $query_run)
 {
-	echo "Could not fetch data successfully".mysql_error();
+	echo "<br>Could not fetch data successfully".mysql_error();
 } 
 else
 {
-	echo "Fetched Data successfully";
+	echo "<br>Fetched Data successfully";
 }
+
+echo "<h1 align='center' style='color:white;'><u>Categories</u></h1>";
+
 
 while($row  = mysql_fetch_assoc($query_run))
 {
 	$category = $row['category'];
 
-	echo "<br><strong><a href=display_category_posts.php?item=$category".">$category</strong>";
+	echo "
+	<html>
+	<head></head>
+	<link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js'></script>
+	<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>
+	<link rel='stylesheet' type='text/css' href='display_categories.css'>
+	<body>
+		<ul class='ulstyle'>
+			<li class='ulstyle_li'>
+				<<br><strong><a href=display_category_posts.php?item=$category><h2><u>$category</u></h2></a></strong>
+			</li>
+		</ul>
+</body>
+</html>"
+;
 
 }
+
+?>
